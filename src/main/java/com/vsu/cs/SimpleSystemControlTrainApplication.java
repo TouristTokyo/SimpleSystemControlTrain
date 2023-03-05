2400fa7e8e73130660ed8cf44b94195a488b0416
Mishka <Fiz-mat228@yandex.ru>
2023-03-05 20:03:09 +0300

    started project

---
+++ src/main/java/com/vsu/cs/SimpleSystemControlTrainApplication.java
@@ -0,0 +1,13 @@
+package com.vsu.cs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSystemControlTrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSystemControlTrainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vsu/cs/SimpleSystemControlTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsu.cs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSystemControlTrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
